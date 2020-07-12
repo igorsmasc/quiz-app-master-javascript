306c38c6a77ffc6c881efe089e19390083bb632a
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-07-12 02:49:45 -0300

    Load and Display High Scores from Local Storage

--- end.js
+++ end.js
@@ -3,6 +3,10 @@ const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -12,4 +16,17 @@ username.addEventListener("keyup", () => {
 
 saveHighScore = (e) => {
   e.preventDefault();
+  console.log("111");
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("/");
 };
